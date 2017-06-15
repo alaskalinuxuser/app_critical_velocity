8e0e2377b4a72dce029ad1d30e087c4cd514092e
AlaskaLinuxUser <alaskalinuxuser@gmail.com>
2017-06-15 09:33:56 -0800

    version 1.3

---
+++ android/assets/convert.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+etc1tool ./bgblue.png -o bgblue.etc1
+etc1tool ./bggreen.png -o bggreen.etc1
+etc1tool ./bottombarrier.png -o bottombarrier.etc1
+etc1tool ./dialogbegin.png -o dialogbegin.etc1
+etc1tool ./dialogempty.png -o dialogempty.etc1
+etc1tool ./dialogspecial.png -o dialogspecial.etc1
+etc1tool ./distfive.png -o distfive.etc1
+etc1tool ./distfour.png -o distfour.etc1
+etc1tool ./distone.png -o distone.etc1
+etc1tool ./distsix.png -o distsix.etc1
+etc1tool ./distthree.png -o distthree.etc1
+etc1tool ./disttwo.png -o disttwo.etc1
+etc1tool ./gameoverBox.png -o gameoverBox.etc1
+etc1tool ./gamewinBox.png -o gamewinBox.etc1
+etc1tool ./powerupbonus.png -o powerupbonus.etc1
+etc1tool ./powerupdistort.png -o powerupdistort.etc1
+etc1tool ./powerup.png -o powerup.etc1
+etc1tool ./powerupslow.png -o powerupslow.etc1
+etc1tool ./superfive.png -o superfive.etc1
+etc1tool ./superfour.png -o superfour.etc1
+etc1tool ./superone.png -o superone.etc1
+etc1tool ./supersix.png -o supersix.etc1
+etc1tool ./superthree.png -o superthree.etc1
+etc1tool ./topbarrier.png -o topbarrier.etc1
+etc1tool ./vehicleexplode.png -o vehicleexplode.etc1
+etc1tool ./vehiclefive.png -o vehiclefive.etc1
+etc1tool ./vehiclefour.png -o vehiclefour.etc1
+etc1tool ./vehicle.png -o vehicle.etc1
+etc1tool ./vehiclesix.png -o vehiclesix.etc1
+etc1tool ./vehiclethree.png -o vehiclethree.etc1
+etc1tool ./vehicletwo.png -o vehicletwo.etc1
+
+
+echo "all done"
+exit 0
